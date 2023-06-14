5fb6a2e236ff6f6a3f9ee5f6117e491fd90fbd66
Masamune <ababiliqbalmuhammad@gmail.com>
2023-06-14 15:25:29 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,77 @@
+const navbar = document.getElementsByTagName('nav')[0];
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 590) {
+        navbar.classList.replace('bg-transparent', 'nav-color');
+    } else if (window.scrollY <= 590) {
+        navbar.classList.replace('nav-color', 'bg-transparent');
+    }
+});
+
+
+if (screen.width >= 992) {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 2,
+        centeredSlides: true,
+        spaceBetween: 30,
+        pagination: {
+            el: ".swiper-pagination",
+            type: "fraction",
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+} else {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        centeredSlides: true,
+        spaceBetween: 30,
+        pagination: {
+            el: ".swiper-pagination",
+            type: "fraction",
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+}
+
+var appendNumber = 4;
+var prependNumber = 1;
+document
+    .querySelector(".prepend-2-slides")
+    .addEventListener("click", function (e) {
+        e.preventDefault();
+        swiper.prependSlide([
+            '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+            '<div class="swiper-slide">Slide ' + --prependNumber + "</div>",
+        ]);
+    });
+document
+    .querySelector(".prepend-slide")
+    .addEventListener("click", function (e) {
+        e.preventDefault();
+        swiper.prependSlide(
+            '<div class="swiper-slide">Slide ' + --prependNumber + "</div>"
+        );
+    });
+document
+    .querySelector(".append-slide")
+    .addEventListener("click", function (e) {
+        e.preventDefault();
+        swiper.appendSlide(
+            '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>"
+        );
+    });
+document
+    .querySelector(".append-2-slides")
+    .addEventListener("click", function (e) {
+        e.preventDefault();
+        swiper.appendSlide([
+            '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+            '<div class="swiper-slide">Slide ' + ++appendNumber + "</div>",
+        ]);
+    });
